627733e7dc973372bb0f07263e9df38d30a15821
manurupan2007 <manurupan2007@gmail.com>
2026-04-17 05:11:56 +0530

    Full overwrite

--- src/components/IntegrationTab.tsx
+++ src/components/IntegrationTab.tsx
@@ -4,7 +4,7 @@ import { Target, Server, ShieldCheck, Activity } from 'lucide-react';
 import { Card, CardContent } from '../components/ui/card';
 
 export default function IntegrationTab() {
-  const discordInviteUrl = "https://discord.com/oauth2/authorize?client_id=1494466348959400168";
+  const discordInviteUrl = "https://discord.com/oauth2/authorize?client_id=1494466348959400168&permissions=68672&integration_type=0&scope=bot";
 
   return (
     <div className="max-w-4xl mx-auto p-4 md:p-8 space-y-8">
